e3e3d52d9de4356eb90b7c2bb033fb89d8d5ea7f
Dominik Wawreczko <dominikwawreczko@gmail.com>
2021-02-20 12:52:47 +0100

    Added Katalog

--- routes/katalog.js
+++ routes/katalog.js
@@ -5,7 +5,7 @@ var dbConn  = require('../lib/db');
 // display books page
 router.get('/', function(req, res, next) {
 
-    dbConn.query('SELECT * FROM Ksiazki ORDER BY id desc',function(err,rows)     {
+    dbConn.query('Select Ksiazki.ID,ISBN, AUTOR, Tytul, Wypozyczajacy  From Ksiazki LEFT JOIN Wypozyczenia ON Ksiazki.ID = Wypozyczenia.ID ORDER BY ID desc',function(err,rows)     {
 
         if(err) {
             // render to views/books/index.ejs
